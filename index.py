8b5849b645ff06ece9b791012e872e721d268cc4
07RRJ <rasmusrudko07@gmail.com>
2024-09-11 11:54:52 +0200

    modul_1

---
+++ index.py
@@ -0,0 +1,24 @@
+#lektion_1
+
+name = "Rasmus"
+age = "17"
+print( name + (" är ") + str(age) + (" år gammal "))
+print('"nuh uh"')
+print("""       yes
+    yes
+yes
+    yes
+        yes
+    yes
+yes
+    yes
+        yes
+    yes
+yes
+    yes""")
+
+
+#lektion_2
+
+namn = input("Skriv ditt namn: ")
+år = input("så " + name + " hur gammal är du? \n")
